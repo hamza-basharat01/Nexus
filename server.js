03417414bd901741aabc4fdba7b553895a62ec80
CodeSandbox <support@codesandbox.io>
2026-06-10 12:09:26 +0000

    Sandbox snapshot commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Nexus Backend Running");
+});
+let users = [];
+
+app.post("/api/register", (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const user = {
+    id: Date.now(),
+    name,
+    email,
+    password,
+    role,
+  };
+
+  users.push(user);
+
+  res.json({
+    message: "User Registered",
+    user,
+  });
+});
+app.post("/api/login", (req, res) => {
+  const { email, password } = req.body;
+
+  const user = users.find((u) => u.email === email && u.password === password);
+
+  if (!user) {
+    return res.status(401).json({
+      message: "Invalid Credentials",
+    });
+  }
+
+  res.json({
+    message: "Login Successful",
+    user,
+  });
+});
+app.get("/api/profile/:id", (req, res) => {
+  const user = users.find((u) => u.id == req.params.id);
+
+  if (!user) {
+    return res.status(404).json({
+      message: "User Not Found",
+    });
+  }
+
+  res.json(user);
+});
+
+app.listen(5000, () => {
+  console.log("Server Running On Port 5000");
+});
